566c02ca2fe4c3276f29403fd72cf1020b789ed0
zvengin <wenjiezhong717@gmail.com>
2024-05-12 22:46:06 +0900

    add scripts

--- toy_train.py
+++ toy_train.py
@@ -103,7 +103,7 @@ def get_loss(model, batch):
 
 
 def get_predictions(model, tokenizer, batch,do_sample):
-    logits = model.(
+    logits = model(
         input_ids= batch['input_ids'].to(model.device),
         attention_mask=batch['attention_mask'].to(model.device)
     ).logits.detach()
