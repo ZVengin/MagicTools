683cf01c0a6500d2231f1a94443ba2777ab9fc35
zvengin <wenjiezhong717@gmail.com>
2024-07-12 02:33:17 +0900

    set random seed for GPU

--- MagicTools/train_function.py
+++ MagicTools/train_function.py
@@ -67,6 +67,8 @@ class TrainUtils:
 
     def set_random_seed(self,random_seed):
         torch.manual_seed(random_seed)
+        torch.cuda.manual_seed(random_seed)
+        torch.cuda.manual_seed_all(random_seed)
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
         np.random.seed(random_seed)
